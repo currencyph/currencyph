a24abbd8d6baf1a203d7813cfb43a7b1e5c037b5
asad <shaikhasad183@gmail.com>
2024-06-02 18:54:04 +0500

    mint and transfer

--- contracts/ETHPH.sol
+++ contracts/ETHPH.sol
@@ -32,34 +32,42 @@ contract ETHPH is ERC20, ERC20Permit, Ownable {
     event RewardClaimed(address indexed user, uint256 amount);
     event TokensReleased(uint256 amount);
 
-    constructor()
-        ERC20("ETHPH", "ETHPH")
-        ERC20Permit("ETHPH")
-        Ownable()
-    {
-        // Mint 21 million tokens initially
-        _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+ constructor()
+    ERC20("ETHPH", "ETHPH")
+    ERC20Permit("ETHPH")
+    Ownable()
+{
+    // Mint 5 million tokens initially to two founding wallet addresses
+    uint256 initialSupply = 5000000 * (10 ** uint256(decimals()));
+    _mint(0xbbd43b65f4d6f4e14efc81d3449fa1b2256a1895, initialSupply / 2);
+    _mint(0x5931d84905cfe25c2c8dea3177dbf7a36a6355df, initialSupply / 2);
 
-        // 10 wallet addresses with correct checksums
-        address payable[] memory wallets = new address payable[](10);
-        wallets[0] = payable(0x7cc58ecBE95c77DBd64f5159c45c12CA6595453D);
-        wallets[1] = payable(0xbC935693A79a997CfCcbBdaE8524F6221387Ff93);
-        wallets[2] = payable(0x7Dc6525B6AA316B388a19C54fBB0f0fa0cb128e7);
-        wallets[3] = payable(0x98d45A79516566B7E9dB045433CEece8e00A4DdB);
-        wallets[4] = payable(0x18f1661a70502caa10DAaA495f391F75214baF49);
-        wallets[5] = payable(0xc04916675801e1A2EB7668E517921a3C5691Ee21);
-        wallets[6] = payable(0x115C808DC1BE36A99cCD4f145eB4E0875079A021);
-        wallets[7] = payable(0xaF9162C8258Bc1C840223469D3a6b272792bD34c);
-        wallets[8] = payable(0x49D9810Ddca06fb0aE8d89b43E197Ebd364BeD2c);
-        wallets[9] = payable(0x2f5C7F990e85CF882ED130711B792FE386319979);
-        
-        uint256 amountPerWallet = 1000000 * (10 ** uint256(decimals())); // 1 million tokens per wallet
 
-        // Distribute tokens to each wallet
-        for (uint256 i = 0; i < wallets.length; i++) {
-            _transfer(msg.sender, wallets[i], amountPerWallet);
-        }
-    }
+}
+
+function mintAndTransfer(address wallet, uint256 amount) external onlyOwner {
+    require(wallet != address(0), "Invalid wallet address");
+    require(amount > 0, "Amount must be greater than 0");
+
+    // Check if 30 days have elapsed since the last release
+    require(block.timestamp >= lastReleaseTime.add(releaseInterval), "Cannot mint and transfer yet, release interval not elapsed");
+
+    // Mint tokens to the founding wallet addresses
+    uint256 foundersAmount = amount.div(2); // Split the amount equally between the two founders
+    _mint(0xbbd43b65f4d6f4e14efc81d3449fa1b2256a1895, foundersAmount);
+    _mint(0x5931d84905cfe25c2c8dea3177dbf7a36a6355df, foundersAmount);
+
+    // Transfer tokens to the specified wallet
+    _mint(wallet, amount);
+
+    // Transfer 10 tokens to the specified wallet
+    uint256 ownerFee = 10 * (10 ** uint256(decimals())); // 10 tokens
+    _transfer(wallet, msg.sender, ownerFee);
+
+    // Update last release time
+    lastReleaseTime = block.timestamp;
+}
+
 
     /**
      * @dev Function to buy tokens with ETH
@@ -108,108 +116,7 @@ contract ETHPH is ERC20, ERC20Permit, Ownable {
         emit TokensSold(msg.sender, amount, usdtAmount);
     }
    
-    /**
-     * @dev Function to stake tokens
-     * @param amount The amount of tokens to stake
-     */
-    function stake(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
-
-        // Transfer tokens to contract
-        _transfer(msg.sender, address(this), amount);
-
-        // Update staked balance
-        stakedBalances[msg.sender] = stakedBalances[msg.sender].add(amount);
-
-        // Update staked users list if the user is not already in the list
-        if (stakedBalances[msg.sender] == amount) {
-            stakedUsers.push(msg.sender);
-            totalStakedUsers++;
-        }
-
-        emit Staked(msg.sender, amount);
-    }
-
-    /**
-     * @dev Function to unstake tokens
-     * @param amount The amount of tokens to unstake
-     */
-    function unstake(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(stakedBalances[msg.sender] >= amount, "Insufficient staked balance");
-
-        // Transfer tokens back to user
-        _transfer(address(this), msg.sender, amount);
-
-        // Update staked balance
-        stakedBalances[msg.sender] = stakedBalances[msg.sender].sub(amount);
-
-        // If user unstaked all tokens, remove them from the staked users list
-        if (stakedBalances[msg.sender] == 0) {
-            removeUserFromArray(msg.sender);
-        }
-
-        emit Unstaked(msg.sender, amount);
-    }
-
-    /**
-     * @dev Function to claim rewards
-     */
-    function claimReward() external {
-        require(lastClaimedTime[msg.sender] + claimCooldown <= block.timestamp, "Cooldown period has not elapsed");
-
-        // Calculate reward based on staked balance and time since last claim
-        uint256 reward = (stakedBalances[msg.sender] * rewardRate * (block.timestamp - lastClaimedTime[msg.sender])) / (1 days);
-
-        // Update last claimed time
-        lastClaimedTime[msg.sender] = block.timestamp;
-
-        // Transfer reward to user
-        _transfer(address(this), msg.sender, reward);
-
-        emit RewardClaimed(msg.sender, reward);
-
-        // Select a random user who holds the token
-        address randomUser = getRandomUser();
-        if (randomUser != address(0)) {
-            // Calculate and transfer 5% of the reward to the random user
-            uint256 randomReward = reward.mul(5).div(100);
-            _transfer(address(this), randomUser, randomReward);
-            emit RewardClaimed(randomUser, randomReward);
-        }
-    }
-
-    /**
-     * @dev Function to release tokens to the owner
-     */
-    function releaseTokens() external onlyOwner {
-        require(block.timestamp >= lastReleaseTime.add(releaseInterval), "Release interval not elapsed yet");
-
-        // Update last release time
-        lastReleaseTime = block.timestamp;
-
-        // Transfer tokens to owner
-        _transfer(address(this), owner(), releaseAmount);
-
-        emit TokensReleased(releaseAmount);
-    }
-
-    /**
-     * @dev Internal function to remove user from the staked users array
-     * @param user The address of the user to be removed
-     */
-    function removeUserFromArray(address user) private {
-        for (uint256 i = 0; i < stakedUsers.length; i++) {
-            if (stakedUsers[i] == user) {
-                stakedUsers[i] = stakedUsers[stakedUsers.length - 1];
-                stakedUsers.pop();
-                totalStakedUsers--;
-                break;
-            }
-        }
-    }
-
+  
     /**
      * @dev Internal function to select a random user from the staked users array
      */
